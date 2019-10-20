3eaee1ce8f98bb5bfa9d209324f3f8a8c8c0a6c4
FAVOUR <truelyfavoured@gmail.com>
2019-10-20 07:21:50 +0100

    Implemented the BottomNavigetionView

--- app/src/main/java/com/example/step_by_step/MainActivity.java
+++ app/src/main/java/com/example/step_by_step/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.step_by_step;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +26,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//                setSupportActionBar(toolbar);
+
+
+        NavHostFragment navHost =(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        NavController navController =navHost.getNavController();
+
+        setButtomNav(navController);
+
+    }
+
+
+
+    public void setButtomNav(NavController navController){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+//        menu.add(R.menu.bottom_nav_elemet)
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
